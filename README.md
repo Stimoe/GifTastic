00d9d98b51111dc9d19a5ab1f90e93f56236b295
Stimoe <stimoe3@gmail.com>
2019-09-11 17:11:23 -0700

    added some more css

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # GifTastic
+What the project does
+Why the project is useful
+How users can get started with the project
+Where users can get help with your project
+Who maintains and contributes to the project
+
+Giftastic makes buttons from an array, it also allows users to add buttons.
+It also allows users to click on a button and 10 gifs on that subject will display.
+If the user clicks on one of the gif's it animates.
+If they click again it goes still.
+
+The project is a good example of how to add buttons to a html using javascript.
+
+If anyone is interested they can run the index html and check out the buttons.
+
+Any questions can be sent to me Thomas Stimac, on my porfolio page, at https://stimoe.github.io/Bootstrap-Portfolio/
+This was solely produced by myself, Thomas Stimac
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,47 +1,37 @@
-var gifs=["Hearthstone", "Overwatch", "Starcraft 2", "PlayerUnknown's Battlegrounds","Minecraft", "Diablo III", "World of Warcraft", "Terraria", "The Sims", "StarCraft", "Garry's Mod", "RollerCoaster Tycoon 3", "Half-Life 2", "Civilization V", "The Sims 3", "Guild Wars", "Fortnite"]
+var gifs=["Homer Simpson", "Marge Simpson", "Maggie Simpson", "Abraham Simpson","Apu", "Barney Gumble", "Chief Clancy Wiggum", "Kent Brockman", "Krusty The Clown", "Martin Prince", "Milhouse Van Houten", "Moe Szyslak", "Mr. Burns", "Ned Flanders", "Ralph Wiggum", "Seymour Skinner", "Seymour Skinner"]
 
 
 function displayGif() {
-
     var name = $(this).attr("data-name");
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=1";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+//link to the queryUrl of GIPHY
     $.ajax({ 
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-       
+        //out of the response this pulls the data
        var results= response.data;
-       
-      
+        //this goes through the data and assigns classes and appends the title and rating to the div
        for (var i = 0; i < results.length; i++) {
-      
-     console.log(results)
-      var ratings = results[i].rating;
-        console.log(ratings)
-      
-      var p = $("<p>").text("Rating: " + ratings);
+        var ratings = results[i].rating;
+        var title=results[i].title;
+      var p = $("<p class='card-text'>").text("Rating: " + ratings);
       var gifDiv = $("<img class = 'name gif data-state'>");
-      
+      var titleOfCard =$("<p class='card-text'>").text(title)
       gifDiv.attr("data-state", "still")
       gifDiv.attr("data-still", results[i].images.fixed_width_still.url)
       gifDiv.attr("data-animate", results[i].images.fixed_width.url)
       gifDiv.attr("src", results[i].images.fixed_width_still.url);
-     
       var imgCard= $("<div id='img-on-card'>").append(gifDiv)
         imgCard.append(p)
+        imgCard.prepend(titleOfCard)
         var card=imgCard
-     
-      $("#gifs-displayed").prepend(card)
-      
-     
-     
-    
+      $("#gifs-displayed").prepend(card)  
 }})
 }
+//this checks what state the gif is in, if it is still it changes it to animate from the source, or if animate it makes still
 $(document).on("click", ".gif", function() {
   var state=$(this).attr("data-state")
-  console.log("you clicked the gif")
   if (state === "still") {
     $(this).attr("src", $(this).attr("data-animate"));
     $(this).attr("data-state", "animate");
@@ -50,31 +40,27 @@ $(document).on("click", ".gif", function() {
     $(this).attr("data-state", "still");
   }
 });
-
-
+//this adds a new button when someone enters it into the add gif here field
 function display(){
   $("#buttons-view").empty();
-
-  for (var i = 0; i < gifs.length; i++) {
+  for (var j = 0; j < gifs.length; j++) {
     var a = $("<button>");
     $("a").addClass("gif-btn");
     // a.addClass("gif-btn");
     a.attr("class", "gif-btn")
     // a.attr("class", "data-state")
-    a.attr("data-name", gifs[i]);
-    a.text(gifs[i]);
+    a.attr("data-name", gifs[j]);
+    a.text(gifs[j]);
     $("#buttons-view").append(a)
-}
-}
+}}
+//this gets the value of what they entered in the add a Gif here field
 $("#add-gif").on("click", function(event) {
   console.log("clicked")
   event.preventDefault();
   var newButtons = $("#gif-input").val();
   gifs.push(newButtons);
-  // console.log(gifs)
   display()
 })
+//this checks if a button has been clicked and if so runs the displayGif function
 $(document).on("click", ".gif-btn", displayGif);
-
-
 display()
